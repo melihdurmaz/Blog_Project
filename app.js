2a9b9403ffded97ae389a91c98ff229ca54b842b
melih <durmazmelih.ml@gmail.com>
2024-03-21 23:26:45 +0300

    düzenleme

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const path=require('path')
-const express = require('express')
-const exp = require('constants')
-const expphbs=require('express-handlebars')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const fileUpload=require('express-fileupload')
-
-
-
+const express =require ('express')
+const router=express.Router()
+const  expphbs=require ('express-handlebars')
 const app = express()
 const port = 3000
-const hostname ='127.0.0.1'
+const hostname = '127.0.0.1'
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const moment = require('moment')
+moment.locale('tr')
+const expressSession = require('express-session')
+const connectMongo = require('connect-mongo')
+
 //mongodb bağlanma işlemleri dikkat et 
 mongoose.connect('mongodb://127.0.0.1/nodeblog_db')
 .then(() => console.log('Connected!'))
@@ -20,28 +21,29 @@ app.use(fileUpload())
 //app.use(express.static(path.join(__dirname, 'Public')))
 
  app.use(express.static('Public'))
-/**alttaki iki satırda handlebars ı kullandık */
- app.engine('handlebars', expphbs.engine());
- app.set('view engine', 'handlebars');
-
 
  // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+ app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+
+
+app.engine('handlebars', expphbs.engine());
+app.set('view engine', 'handlebars');
+ // parse application/json
 app.use(bodyParser.json())
 
-//file upload için
+
 
 
  //routers klasorünü kullanmak için klasörü dahil ettik
  const main=require('./routers/main')
- app.use('/',main)   //sayfa yönlendirmeleri için main klasörünü kullandım
- //routers klasorünü kullanmak için klasörü dahil ettik
  const posts=require('./routers/posts')
- app.use('/posts',posts)   //sayfa yönlendirmeleri için posts klasörünü kullandım
-
-
+ const users=require('./routers/users')
+ 
+ app.use('/',main)   
+ app.use('/posts',posts) 
+ app.use('/users',users)
+ //sayfa yönlendirmeleri için posts klasörünü kullandım
 
 
 /**burada ise serverimi görmek için app.listen komutunu kullandım */
